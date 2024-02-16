17396ee7a25503f2667ea0d067645c879e3f5836
yydaydayup <132675630+yydaydayup@users.noreply.github.com>
2024-02-16 12:42:21 +0800

    Update 附加功能.js

--- 附加功能.js
+++ 附加功能.js
@@ -1,3 +1,19 @@
+// 创建星星并添加到body中
+function createStar() {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.animationDuration = `${Math.random() * 3 + 2}s`;
+    star.style.animationName = 'fall';
+    document.body.appendChild(star);
+    
+    // 一段时间后移除星星
+    setTimeout(() => {
+        star.remove();
+    }, star.style.animationDuration.replace('s', '') * 1000);
+}
+
+
 let audio = new Audio('天天 - 陶喆.mp3');
 let playPauseBtn = document.getElementById('playPauseBtn');
 let progressBar = document.getElementById('progressBar');
